2cf3257d0a71aa15a1437e05dc6caff836ad3399
bkaraev98-max <bkaraev98@gmail.com>
2026-07-29 02:43:15 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import telebot
 from telebot import types
 
-TOKEN = "8618711572:AAHYOHM0f4nG-r0VZp3PsuaUI7m5N63Z8ow"
-bot = telebot.TeleBot(TOKEN)
+TOKEN = "8618711572:AAHTpbMmTbTvIyV0mSh838gRxb_sPXTgKWg"
+
 
 
 @bot.message_handler(commands=['start'])
