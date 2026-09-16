e6c5f0fc76fb3f44baaac85e40af557998e21a4f
bkaraev98-max <bkaraev98@gmail.com>
2026-09-16 14:30:54 +0500

    Update permissions.py

--- core/permissions.py
+++ core/permissions.py
@@ -1,16 +1,1 @@
-from enum import IntEnum
 
-
-class Role(IntEnum):
-    USER = 10
-    OPERATOR = 20
-    ADMIN = 30
-    MANAGER = 40
-    OWNER = 50
-
-
-def has_permission(
-    role: Role,
-    required_role: Role,
-) -> bool:
-    return role >= required_role
